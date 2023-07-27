a49f36e33345585306c8b0c3cc5bcd1792366037
Danuel Williams <joseph_thesaint@hotmail.com>
2023-07-27 18:45:23 -0500

    Updates Question Management Page to pull directly from API instead of having canned items

--- ias/pages/admin/question-management/index.vue
+++ ias/pages/admin/question-management/index.vue
@@ -2,50 +2,45 @@
   <BasePage>
     <div class="flex flex-col">
       <p class="font-extrabold text-5xl">Question Management</p>
-
-      <div class="box w-1/2">
-        <p>Do you have any food, plants or other organic materials travelling with you<br></p>
-        <p>Weight: 36<p/>
+      <p> Add New Question</p>
+      <router-link to="/admin/question-management/ai-generation">Generate New Questions</router-link>
+      <div class="box w-1/2" v-for="question in questions">
+        <p>{{ question.question }}<br></p>
+        <p>Category: {{ categoryNameFromSlug(question.category)}}</p>
+        <p>Weight: {{question.weight}}<p/>
         <div class="flex flex-row gap-8">
           <router-link to="/admin/question-management/edit" class="blue-btn flex-1">Edit</router-link>
-          <p class="red-btn flex-1">Delete</p>
-        </div>
-      </div>
-      <div class="box w-1/2">
-        <p>Do you have any food, plants or other organic materials travelling with you<br></p>
-        <p>Weight: 16<p/>
-        <div class="flex flex-row gap-8">
-          <p class="blue-btn flex-1">Edit</p>
-          <p class="red-btn flex-1">Delete</p>
-        </div>
-      </div>
-      <div class="box w-1/2">
-        <p>Do you have any food, plants or other organic materials travelling with you<br></p>
-        <p>Weight: 49<p/>
-        <div class="flex flex-row gap-8">
-          <p class="blue-btn flex-1">Edit</p>
-          <p class="red-btn flex-1">Delete</p>
-        </div>
-      </div>
-      <div class="box w-1/2">
-        <p>Do you have any food, plants or other organic materials travelling with you<br></p>
-        <p>Weight: 25<p/>
-        <div class="flex flex-row gap-8">
-          <p class="blue-btn flex-1">Edit</p>
-          <p class="red-btn flex-1">Delete</p>
-        </div>
-      </div>
-      <div class="box w-1/2">
-        <p>Do you have any food, plants or other organic materials travelling with you<br></p>
-        <p>Weight: 50<p/>
-        <div class="flex flex-row gap-8">
-          <p class="blue-btn flex-1">Edit</p>
           <p class="red-btn flex-1">Delete</p>
         </div>
       </div>
     </div>
   </BasePage>
 </template>
+
+<script>
+export default  {
+  methods: {
+    categoryNameFromSlug(slug) {
+      let categoryName = this.$store.getters['categories/getAllCategories'].find((category) => {
+        return category.slug === slug;
+      })
+      if (categoryName === undefined) {
+        return "Unknown Category"
+      }
+      return categoryName.name;
+    }
+  },
+  computed: {
+    questions() {
+      return this.$store.getters['questions/getAllQuestions'];
+    }
+  },
+  beforeMount() {
+    this.$store.dispatch('questions/retrieveQuestions');
+    this.$store.dispatch('categories/retrieveCategories');
+  }
+}
+</script>
 
 <style>
 .box > *{
