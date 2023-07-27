d415296597049eaf10482461ab6ca6bde1ba001a
Danuel Williams <joseph_thesaint@hotmail.com>
2023-07-27 02:50:41 -0500

    Implements chatGPT prompt API

--- aips-server/server.js
+++ aips-server/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
+import 'dotenv/config';
+import http from 'node:http';
+import https from 'node:https';
 
 //MODELS
 import * as model from './db/model/models.js'
@@ -40,6 +43,55 @@ app.get('/api/purposes-of-visit', (req,res) => {
     let allPurposesOfVisits = model.PurposeOfVisit.find({}).exec().then((result) => {
         res.send(result);
     })
+})
+
+app.post('/api/prompt-chatgpt', (req, res, next) => {
+    let openai_request_body = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {
+                "role": "user",
+                "content": req.body.content
+            }
+        ],
+        "temperature": 0.7
+    }
+    let request_options = {
+        host: 'api.openai.com',
+        path: '/v1/chat/completions',
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+            'Content-Type': 'application/json',
+            'OpenAI-Organization': `${process.env.OPENAI_ORG_KEY}`,
+            'Connection': 'keepalive'
+        }
+    }
+
+    function process_response(response) {
+        // TODO: Will have to parse the response as
+        //  json for content type reasons at some point
+        console.log("processing response");
+        let str = '';
+        let errstr = '';
+        response.on('data', function(chunk) {
+            str += chunk;
+        });
+        response.on('end', function() {
+            res.send(JSON.parse(str));
+        })
+        response.on('error', function(error) {
+            res.send(error);
+        })
+    }
+    try {
+        let chatGPTRequest = https.request(request_options, process_response);
+        console.log("Request Body:\n", openai_request_body);
+        chatGPTRequest.write(JSON.stringify(openai_request_body));
+        chatGPTRequest.end();
+    } catch (error) {
+        next(error);
+    }
 })
 
 const PORT = process.env.PORT || 8080;
