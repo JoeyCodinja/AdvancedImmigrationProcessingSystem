8ad20b9940edb1d08e3f1328b4e47b0c311bb99b
Danuel Williams <joseph_thesaint@hotmail.com>
2023-07-30 17:11:40 -0500

    Includes property for title in the BasePage component

--- ias/components/Sidebar.vue
+++ ias/components/Sidebar.vue
@@ -2,56 +2,59 @@
   <div class="bg-black text-white min-w-fifth min-h-full flex flex-col flex-full">
     <router-link to="/"><img src="/pica_logo-removebg-preview.png"/></router-link>
     <div class="flex flex-row items-center flex-two">
-      <p class="flex-1 sep-right"><font-awesome-icon icon="fa-solid fa-bars"/>Menu</p>
-      <p class="flex-1"><font-awesome-icon icon="fa-solid fa-magnifying-glass"/>Search</p>
+      <p class="flex-1 sep-right text-center"><font-awesome-icon icon="fa-solid fa-bars"/>Menu</p>
+      <p class="flex-1 sep-left text-center"><font-awesome-icon icon="fa-solid fa-magnifying-glass"/>Search</p>
     </div>
-    <router-link to="/cases" v-if="$route.path!=='/cases'"><img src=""/>Cases</router-link>
-    <router-link to="/cases" v-if="$route.path==='/cases'" class="text-yellow-400"><img src=""/>Cases</router-link>
-    <p v-if="!$route.path.includes('admin')">
+    <router-link to="/cases" v-if="$route.path!=='/cases'" class="flex-1 pl-4"><img src=""/>Cases</router-link>
+    <router-link to="/cases" v-if="$route.path==='/cases'" class="text-yellow-400 flex-1 pl-4"><img src=""/>Cases</router-link>
+    <p v-if="!$route.path.includes('admin')" class="flex-1 pl-4">
       <font-awesome-icon
         icon="fa-solid fa-gear"/>
       Settings
     </p>
-    <p v-if="$route.path.includes('admin')" class="text-yellow-400">
+    <p v-if="$route.path.includes('admin')" class="text-yellow-400 flex-1 pl-4">
       <font-awesome-icon
         icon="fa-solid fa-gear"
-        class="text-yellow-400" />
+        class="text-yellow-400 pl-4" />
       Settings
     </p>
     <router-link
       to="/admin/generative-resources"
-      v-if="$route.path!=='/admin/generative-resources'">
+      v-if="$route.path!=='/admin/generative-resources'"
+      class="flex-1 pl-4">
       Training Data
     </router-link>
     <router-link
       to="/admin/generative-resources"
-      v-if="$route.path==='/admin/generative-resources'" class="text-yellow-400">
+      v-if="$route.path==='/admin/generative-resources'" class="text-yellow-400 flex-1 pl-4">
       Training Data
     </router-link>
 
     <router-link
       v-if="$route.path!=='/admin/question-management'"
-      to="/admin/question-management">
+      to="/admin/question-management"
+      class="flex-1 pl-4">
       Question Management
     </router-link>
     <router-link
       to="/admin/question-management"
-      v-if="$route.path==='/admin/question-management'" class="text-yellow-400">
+      v-if="$route.path==='/admin/question-management'" class="text-yellow-400 flex-1 pl-4">
       Question Management
     </router-link>
 
     <router-link
       to="/admin/prompt-engineering"
-      v-if="$route.path!=='/admin/prompt-engineering'">
+      v-if="$route.path!=='/admin/prompt-engineering'"
+      class="flex-1 pl-4">
       Prompt Engineering
     </router-link>
     <router-link
       to="/admin/prompt-engineering"
-      v-if="$route.path==='/admin/prompt-engineering'" class="text-yellow-400">
+      v-if="$route.path==='/admin/prompt-engineering'" class="text-yellow-400 flex-1 pl-4">
       Prompt Engineering
     </router-link>
 
-    <div class="flex flex-row items-center">
+    <div class="flex flex-row items-center pl-4">
       <img src="/power-32.png"/>
       Logout
     </div>
@@ -66,6 +69,10 @@ export default {
 
 <style>
 .sep-right {
-  border-right: 2px white solid;
+  border-right: 1px white solid;
+}
+
+.sep-left {
+  border-left: 1px white solid;
 }
 </style>
