0388ce0c8846e83bcbc096e75cf73591d868edee
Danuel Williams <joseph_thesaint@hotmail.com>
2023-08-06 10:53:43 -0500

    Collection name was wrong for Entrant model

--- aips-server/db/model/models.js
+++ aips-server/db/model/models.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import  * as schemas from "../schema/schemas.js";
 
 export const Question = mongoose.model('Question', schemas.questionSchema, 'question');
-export const Entrant = mongoose.model('Entrant', schemas.entrantSchema, 'entrants.js');
+export const Entrant = mongoose.model('Entrant', schemas.entrantSchema, 'entrants');
 export const Entry = mongoose.model('Entry', schemas.entrySchema, 'entry');
 export const PurposeOfVisit = mongoose.model('PurposeOfVisit', schemas.purposeOfVisitSchema, 'purpose_of_visit')
 export const Category = mongoose.model('Category', schemas.categorySchema, 'category');
