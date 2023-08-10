7a89514c26446d83da89f0f76bbc865f951e9e23
Danuel Williams <joseph_thesaint@hotmail.com>
2023-08-09 22:50:10 -0500

    General Styling changes

--- ias/tailwind.config.js
+++ ias/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       },
       flex: {
         'half': '0 0 4rem',
+        '1.5': '1.5 0 2rem',
         'two': '2 0 2rem',
         '2': '2 0 2rem',
         'full': '5 0 2rem'
