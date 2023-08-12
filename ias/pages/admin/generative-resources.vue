2883cee47c614b560984959e823a71090cdadf33
Danuel Williams <joseph_thesaint@hotmail.com>
2023-08-12 03:03:28 -0500

    Initial work for pre-eval questions being added to safety score

--- ias/pages/admin/generative-resources.vue
+++ ias/pages/admin/generative-resources.vue
@@ -1,26 +1,41 @@
 <template>
   <BasePage title="Current Training Data Documents">
-    <div class="flex flex-col">
-      <p style="padding-bottom: 2rem;display:block;" class="text-2xl">
-        These documents are use to form the context
+    <div class="apple-container">
+      <p class="apple-title">
+        These documents are used to form the context
         through which the questions in the system
         are generated
       </p>
-      <div style="display: flex; ">
-        <div style="flex: 1 1 100%; margin-right:5%;">
-          <p class="documents-title text-lg font-bold">Documents in use</p>
-          <ul>
-            <li>Immigration Restriction (Commonwealth Citizens Act (1945))</li>
-            <li>Aliens Act (1946)</li>
-            <li>Passport Act</li>
-            <li>Foreign Nationals and Commonwealth Citizens (Employment Act)</li>
-            <li>Nationality Act</li>
+      <div class="apple-documents">
+        <div class="apple-documents-list">
+          <p class="apple-documents-heading">Documents in use</p>
+          <ul class="apple-ul">
+            <li class="apple-li">
+              Immigration Restriction (Commonwealth Citizens Act (1945))
+              <img src="/edit-pencil.png" class="w-5 h-5" />
+            </li>
+            <li class="apple-li">
+              Aliens Act (1946)
+              <img src="/edit-pencil.png" class="w-5 h-5" />
+            </li>
+            <li class="apple-li">
+              Passport Act
+              <img src="/edit-pencil.png" class="w-5 h-5" />
+            </li>
+            <li class="apple-li">
+              Foreign Nationals and Commonwealth Citizens (Employment Act)
+              <img src="/edit-pencil.png" class="w-5 h-5" />
+            </li>
+            <li class="apple-li">
+              Nationality Act
+              <img src="/edit-pencil.png" class="w-5 h-5" />
+            </li>
           </ul>
         </div>
-        <div style="flex: 1 1 100%;">
-          <p class="upload-title text-lg font-bold" style="display:block;text-align:center;">Upload Documents</p>
-          <img src="/document-outline/document-outline/64x64.png" style="margin-left:auto;margin-right:auto;"/>
-          <button style="margin-left:auto;margin-right:auto;display:block;">Browse</button>
+        <div class="apple-upload">
+          <p class="apple-documents-heading">Upload Documents</p>
+          <img src="/document-outline/document-outline/64x64.png" class="apple-upload-icon"/>
+          <button class="apple-upload-button">Browse</button>
         </div>
       </div>
     </div>
@@ -32,18 +47,96 @@
 </script>
 
 <style>
-ul > li {
-  border-bottom: 1px solid black;
-  padding-bottom: 10px;
+.apple-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 3rem;
 }
 
-ul > li:last-child {
-  border-bottom: 0px;
-  padding-bottom: 0px;
-}
-
-.documents-title, upload-title {
-  display: inherit;
+.apple-title {
+  font-size: 1.5rem;
   text-align: center;
+  margin-bottom: 2rem;
+}
+
+.apple-documents {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+}
+
+.apple-documents-list, .apple-upload {
+  flex: 1;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  margin: 0 1rem;
+  background-color: white;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.apple-documents-heading {
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 1rem;
+}
+
+.apple-ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+.apple-li {
+  margin-bottom: 0.5rem;
+  padding-left: 1rem;
+  position: relative;
+  line-height: 1.4;
+  border-bottom: 1px solid #ccc; /* Add border line between items */
+  padding-bottom: 0.5rem; /* Add padding to separate items */
+  display: flex;
+  align-items: center;
+}
+
+.apple-li img {
+  margin-left: auto;
+}
+
+.apple-li:last-child {
+  border-bottom: none; /* Remove border line from last item */
+  padding-bottom: 0; /* Remove padding from last item */
+}
+
+.apple-li::before {
+  content: "•";
+  position: absolute;
+  left: 0;
+  top: 0.2rem;
+  color: #007bff;
+}
+
+.apple-upload-icon {
+  display: block;
+  margin: 0 auto;
+}
+
+.apple-upload-button {
+  display: block;
+  margin: 1rem auto;
+  padding: 0.5rem 1rem;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.apple-upload-button:hover {
+  background-color: #0056b3;
 }
 </style>
--- ias/pages/cases/review/index.vue
+++ ias/pages/cases/review/index.vue
@@ -1,345 +1,174 @@
 <template>
-  <BasePage title="Case - Review" caseReview="true">
-    <div class="flex flex-row flex-1">
-      <div class="flex flex-col flex-wrap w-one-third p-4">
+  <BasePage title="Cases">
+    <div class="flex flex-col justify-center items-center h-full">
+      <router-link class="create-case-button" to="/cases/new">Create New Case</router-link>
+      <div v-for="n in 4" class="cases-box">
         <div class="flex flex-row">
-          <!-- Entrant image with border -->
-          <div class="entrant-image items-center">
-            <img src="/entrant-headshot.png" class="">
+          <div class="flex-1 entrant-image">
+            <img src="/entrant-headshot.png">
           </div>
-        </div>
-        <div class="flex flex-col mt-4 items-center">
-          <!-- Entrant details -->
-            <div class="entrant-safety-score">
-              <p class="safety-score-label">Safety Score</p>
-              <span class="text-4xl font-bold"
-                    v-bind:class="safety_rating_style(safety_rating)">
-                {{ safety_rating }}
-              </span>
+          <div class="flex flex-row flex-2 entrant-info">
+            <div class="info-item risk-item">
+              Risk Rating: <span class="risk-rating good-risk">98</span>
             </div>
-            <p class="entrant-name ">{{entrant.first_name}} {{entrant.last_name}}</p>
-
-            <p class="entrant-detail">Arriving From: <span class="font-bold">MIA</span></p>
-            <p class="entrant-detail">Nationality: <span class="font-bold">Jamaican</span></p>
-            <p class="entrant-detail">Status: <span class="font-bold">Pending</span></p>
-          </div>
-          <div class="flex flex-row">
-            <!-- Toggling sections -->
-            <p class="flex flex-1 bottom-border
-                      text-center text-base font-bold
-                      right-border tab-padding"
-               v-on:click="toggle('overview')">Overview</p>
-            <p class="flex flex-1 bottom-border
-                      text-center text-base font-bold
-                      tab-padding"
-               v-on:click="toggle('travel_history')">Travel History</p>
-          </div>
-          <div v-show="show_overview">
-            <!-- Entrant overview -->
-            <EntrantOverview v-bind:entrant="entrant"/>
-          </div>
-          <div v-show="show_travel_history">
-            <!-- Entrant Travel History -->
-            <EntrantHistory v-bind:history="entrantHistory"/>
-          </div>
-        </div>
-        <div class="flex flex-col flex-1 w-two-third p-4">
-      <!-- Rest of the code -->
-
-      <div class="flex flex-col">
-        <p class="text-2xl">Interview Questions</p>
-        <div class="mt-3 flex flex-col" v-if="fetching_questions == true">
-          Fetching Questions
-          <font-awesome-icon icon="fa-solid fa-spinner" spin/>
-        </div>
-        <div class="mt-3 flex flex-col" v-if="interviewQuestions.length === 0 && fetching_questions == false">
-          All interview questions asked
-        </div>
-        <div class="questions-box" v-for="(question, index) of interviewQuestions" :key="question.id">
-          <div class="flex flex-row">
-            <p class="text-base basis-20 flex-2 font-semibold border-b pb-3">{{ question.question }}</p>
-            <div class="ml-auto">
-              <button class="rounded-2xl border bg-yellow-300 px-3 py-1 text-xs font-bold">
-                Weight: {{ question.weight }}
-              </button>
+            <div class="info-details ml-6 flex-1">
+              <div class="info-column">
+                <div class="info-row">
+                  <div class="info-name">First Name:</div>
+                  <div class="info-value">Derek</div>
+                </div>
+                <div class="info-row">
+                  <div class="info-name">Last Name:</div>
+                  <div class="info-value">Jeter</div>
+                </div>
+              </div>
+              <div class="info-column">
+                <div class="info-row">
+                  <div class="info-name">Arriving From:</div>
+                  <div class="info-value">MIA</div>
+                </div>
+                <div class="info-row">
+                  <div class="info-name">Nationality:</div>
+                  <div class="info-value">Jamaican</div>
+                </div>
+              </div>
             </div>
           </div>
-          <textarea v-bind:ref="'canned_response_open_' + index"
-                    v-bind:name="'canned_response_open_' + index"
-                    v-if="!question.canned_response"/>
-                    <div v-else>
-          <div class="canned-buttons">
-            <label for="canned_response_yes" class="canned-label">
-              <input v-bind:ref="'canned_response_yes_' + index"
-                    v-bind:name="'canned_response_group_' + index "
-                    type="radio" class="canned-radio-button"/> Yes
-            </label>
-
-            <label for="canned_response_no" class="canned-label">
-              <input v-bind:ref="'canned_response_no_' + index"
-                    v-bind:name="'canned_response_group_' + index "
-                    type="radio" class="canned-radio-button"/> No
-            </label>
-          </div>
-        </div>
-          <div class="question mt-4">
-            <button class="accept-button" v-on:click="addAnsweredQuestion(question, index, true)">
-              Acceptable
-            </button>
-            <button class="unacceptable-button" v-on:click="addAnsweredQuestion(question, index, false)">
-              Unacceptable
-            </button>
-            <button class="skip-button" v-on:click="addAnsweredQuestion(question, index, null)">Skip</button>
+          <div class="review-link">
+            <router-link to="/cases/review">
+              Review
+            </router-link>
           </div>
         </div>
       </div>
     </div>
-  </div>
   </BasePage>
 </template>
 
 <script>
-export default{
-  data() {
-    return {
-      show_overview: true,
-      show_travel_history: false,
-      fetching_questions: true
-    }
-  },
-  computed: {
-    entrant() {
-      return this.$store.getters['entrants/getCurrentEntrant'];
-    },
-    entrantHistory() {
-      return this.$store.getters['entrants/getCurrentEntrantHistory'].slice(1);
-    },
-    interviewQuestions() {
-      return this.$store.getters['questions/getAllInterviewQuestions'];
-    },
-    latestEntrantHistory() {
-      return this.$store.getters['entrants/getLatestEntry'];
-    },
-    safety_rating: {
-      get() {
-        return this.$store.getters['entrants/getLatestEntry'].safety_rating;
-      },
-      set(new_safety_rating) {
-        this.$store.commit('entrants/updateLatestEntrySafetyRating', new_safety_rating);
-      }
-    }
-  },
+export default {
   methods: {
-    toggle(panel) {
-      let toggles = Object.keys(this.$data).filter((key) => key.includes('show'));
-      let panelsToDeToggle = toggles.filter((key) => !key.includes(panel));
-      this.$data[`show_${panel}`] = true;
-      panelsToDeToggle.forEach((panel) => {
-        console.log(`Detoggling: ${panel}`);
-        this.$data[panel] = false;
-      })
-    },
-    addAnsweredQuestion(question, index, add){
-      // Pull ref
-      let answeredQuestion = JSON.parse(JSON.stringify(question));
-      answeredQuestion['skipped'] = false;
-      if (question.canned_response){
-        let responseYes = this.$refs[`canned_response_yes_${index}`][0].checked;
-        let responseNo = this.$refs[`canned_response_no_${index}`][0].checked;
-        if (responseYes){
-          answeredQuestion['response'] = "Yes";
-        } else if (responseNo) {
-          answeredQuestion['response'] = "No";
-        } else {
-          answeredQuestion['response'] = "No answer given";
-          if (add === null){
-            answeredQuestion['skipped'] = true;
-            this.generateNewQuestion(question.id);
-          }
-        }
-      } else {
-        let response = this.$refs[`canned_response_open_${index}`][0].value;
-        if (response.length == 0){
-          answeredQuestion['response'] = "No answer given";
-          if (add === null){
-            answeredQuestion['skipped'] = true;
-            this.generateNewQuestion(question.id);
-          }
-        } else {
-          answeredQuestion['response'] = this.$refs[`canned_response_open_${index}`][0].value;
-        }
-      }
-      this.$store.commit('entrants/addAnsweredQuestion', answeredQuestion);
-      this.changeSafetyRating(question.weight, add);
-      this.$store.dispatch('questions/removeQuestion', question.id);
-    },
-    changeSafetyRating(weight, add) {
-      if (add) {
-        this.safety_rating += weight
-      } else if (add == false) {
-        this.safety_rating -= weight
-      }
-    },
-    generateNewQuestion(questionId) {
-      this.$store.dispatch('questions/retrieveInterviewQuestion', this.entrant.id);
-    },
-    safety_rating_style(safetyRatingValue) {
-      let safety_rating_class = ""
-      if (safetyRatingValue <= 25) {
-        safety_rating_class = 'bad-risk';
-      }
-      else if (safetyRatingValue >= 26 && safetyRatingValue <= 50) {
-        safety_rating_class = 'tentative-risk';
-      }
-      else if (safetyRatingValue >= 51 && safetyRatingValue <= 75) {
-        safety_rating_class =  'ok-risk';
-      }
-      else if (safetyRatingValue >= 76 && safetyRatingValue <= 100) {
-        safety_rating_class =  'good-risk';
-      } else {
-        safety_rating_class = 'unknown-risk';
-      }
-      return safety_rating_class;
-    }
   },
   beforeMount() {
-    this.$store.dispatch('entrants/findEntrant', "F77698918").then(() => {
-      this.$store.dispatch('questions/retrieveInterviewQuestions', this.entrant.id).then(() => {
-        this.fetching_questions = false;
-      });
-    });
-  },
-  mounted() {
   }
 }
 </script>
 
 <style scoped>
-.bottom-border {
-  border-bottom: 2px black solid;
+.create-case-button {
+  background-color: #333;
+  color: white;
+  margin-bottom: 12px;
+  padding: 8px 16px;
+  display: inline-block;
+  text-decoration: none;
+  border-radius: 8px;
+  transition: background-color 0.3s, color 0.3s;
 }
 
-.w-one-third {
-  width: 33.33333%;
+.create-case-button:hover {
+  background-color: #444;
 }
 
-.w-two-third {
-  width: 66.66667%;
-}
-
-.p-4 {
+.cases-box {
+  margin: 1.5rem;
   padding: 1rem;
-}
-
-.right-border {
-  border-right: 2px black solid;
-}
-
-.tab-padding {
-  padding-left: 2rem;
-}
-
-
-.entrant-image {
-  border: 5px solid #0b5688; /* Blue color */
-  padding: 1rem; /* Padding to create space between the image and the border */
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); /* Shadow effect */
-  transition: border-color 0.3s, box-shadow 0.3s; /* Smooth transitions */
-}
-.entrant-image:hover {
-  border-color: #0c0fbe; /* Red color on hover */
-  box-shadow: 0 0 15px rgba(231, 76, 60, 0.4); /* Bigger shadow on hover */
-}
-
-.entrant-detail {
-  font-size:large;
-}
-
-.entrant-name {
-  font-size: 2rem;
-  font-weight:700;
-  margin-bottom: 0.5rem;
-  color: #333;
-}
-
-.entrant-safety-score {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 1rem;
-}
-
-.safety-score-label {
-  font-size: 2.5rem;
-  font-weight: bold;
-  margin-bottom: 0.5rem;
-  color: #333;
-}
-
-.questions-box {
-  margin: 0 auto;
-  margin-top: 1.5rem;
-  padding: 1rem;
-  border-radius: 1rem;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.35);
+  border-radius: 12px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
   width: 100%;
   background-color: white;
+  display: flex;
+  flex-direction: column;
+  transition: box-shadow 0.3s;
 }
 
-.accept-button
-,.unacceptable-button
-,.skip-button {
-  background-color:black;
-  border: none;
-  color: white;
-  font-weight: bold;
-  padding: 0.5rem 1.5rem;
-  border-radius: 0.375rem;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
-  transition: background-color 0.3s, box-shadow 0.3s;
-  margin-right:2rem;
+.cases-box:hover {
+  box-shadow: 0px 6px 8px rgba(0, 0, 0, 0.3);
 }
 
-.accept-button:hover {
-  background-color: #13ac40;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+.cases-box .flex-row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 
-.unacceptable-button:hover {
-  background-color: #c21313;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+.entrant-image {
+  flex: 1;
+  border-top-left-radius: 12px;
+  border-bottom-left-radius: 12px;
+  overflow: hidden;
 }
 
-.skip-button:hover {
-  background-color: #f6e05e;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
-}
-
-textarea {
-  background-color: lightgray;
+.entrant-image img {
   width: 100%;
+  height: auto;
+  display: block;
+  transition: transform 0.3s;
 }
 
-.canned-radio-button {
-  appearance: none;
-  width: 20px;
-  height: 20px;
-  border: 2px solid #999;
-  border-radius: 50%;
-  transition: border-color 0.3s, background-color 0.3s;
-  cursor: pointer;
-  margin-right: 0.5rem;
+.entrant-image img:hover {
+  transform: scale(1.05);
 }
 
-.canned-radio-button:checked {
-  border-color: #0c0fbe;
-  background-color: #0c0fbe;
-}
-
-.canned-label {
+.entrant-info {
+  flex: 3;
   display: flex;
   align-items: center;
-  cursor: pointer;
-  font-weight:500;
+  padding-left: 1rem;
+  border-left: 2px solid #ddd;
+}
+
+.info-item {
+  margin: 0;
+  margin-bottom: 0.5rem;
+}
+
+.info-column {
+  display: flex;
+  flex-direction: column;
+  margin-right: 1.5rem;
+}
+
+.info-row {
+  display: flex;
+  align-items: center;
+}
+
+.info-name {
+  margin: 0;
+  color: #666;
+  font-weight: bold;
+  width: 120px;
+}
+
+.info-value {
+  margin: 0;
+}
+
+.risk-item {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+  color: #666;
+}
+
+.risk-rating {
+  font-weight: bold;
+  margin-left: 0.5rem;
+  color: green;
+}
+
+.review-link {
+  background-color: #333;
+  color: white;
+  padding: 8px 16px;
+  text-decoration: none;
+  display: inline-block;
+  border-radius: 8px;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+.review-link:hover {
+  background-color: #444;
 }
 </style>
