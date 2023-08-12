0b0c74e13201e6b300d0028e423d117b38347174
Danuel Williams <joseph_thesaint@hotmail.com>
2023-08-12 06:32:33 -0500

    Updates from Andre

--- ias/pages/index.vue
+++ ias/pages/index.vue
@@ -1,19 +1,165 @@
 <template>
   <BasePage>
-    <SummaryBlock>
-      Persons denied entry today: <br><br> 9
-    </SummaryBlock>
-    <SummaryBlock>
-      Persons accepted entry today: <br><br> 100
-    </SummaryBlock>
-    <SummaryBlock>
-      Persons accessing our ports today: <br><br> 109
-    </SummaryBlock>
+    <div class="h-full ">
+
+    <!-- Statistics Cards -->
+    <div class="grid  lg:grid-cols-4 p-4 gap-32">
+      <div class="bg-gray-800 shadow-lg rounded-md flex items-center justify-between px-6 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
+        <div class="text-right">
+          <p class="text-2xl">9,257</p>
+          <p>Travellers Approved for Entry</p>
+        </div>
+      </div>
+      <div class="bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
+        <div class="text-right">
+          <p class="text-2xl">15</p>
+          <p>Travellers Referred For Further Processing</p>
+        </div>
+      </div>
+      <div class="bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
+        <div class="flex justify-center items-center  transition-all duration-300 transform group-hover:rotate-12">
+        </div>
+        <div class="text-right">
+          <p class="text-2xl">7</p>
+          <p>Travellers Denied for Entry</p>
+        </div>
+      </div>
+      <div class="bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
+        <div class="flex justify-center items-center  transition-all duration-300 transform group-hover:rotate-12">
+        </div>
+        <div class="text-right">
+          <p class="text-2xl">10,257</p>
+          <p>Travellers Visiting the Country</p>
+        </div>
+      </div>
+    </div>
+    <!-- ./Statistics Cards -->
+
+    <!-- Client Table -->
+    <div class="mt-4 mx-4">
+      <div class="w-full overflow-hidden rounded-lg shadow-xs">
+        <div class="w-full overflow-x-auto">
+          <table class="w-full">
+            <thead>
+              <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                <th class="px-4 py-3">Client</th>
+                <th class="px-4 py-3">Status</th>
+                <th class="px-4 py-3">Date</th>
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+              <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
+                <td class="px-4 py-3">
+                  <div class="flex items-center text-sm">
+                    <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
+                      <img class="object-cover w-full h-full rounded-full" src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=200&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjE3Nzg0fQ" alt="" loading="lazy" />
+                      <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                    </div>
+                    <div>
+                      <p class="font-semibold">Hans Burger</p>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-4 py-3 text-xs">
+                  <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"> Approved </span>
+                </td>
+                <td class="px-4 py-3 text-sm">12-08-2023</td>
+              </tr>
+              <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
+                <td class="px-4 py-3">
+                  <div class="flex items-center text-sm">
+                    <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
+                      <img class="object-cover w-full h-full rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=200&amp;facepad=3&amp;fit=facearea&amp;s=707b9c33066bf8808c934c8ab394dff6" alt="" loading="lazy" />
+                      <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                    </div>
+                    <div>
+                      <p class="font-semibold">Jolina Angelie</p>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-4 py-3 text-xs">
+                  <span class="px-2 py-1 font-semibold leading-tight text-yellow-700 bg-yellow-100 rounded-full"> Referred </span>
+                </td>
+                <td class="px-4 py-3 text-sm">12-08-2023</td>
+              </tr>
+              <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
+                <td class="px-4 py-3">
+                  <div class="flex items-center text-sm">
+                    <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
+                      <img class="object-cover w-full h-full rounded-full" src="https://images.unsplash.com/photo-1502720705749-871143f0e671?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=200&amp;fit=max&amp;s=b8377ca9f985d80264279f277f3a67f5" alt="" loading="lazy" />
+                      <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                    </div>
+                    <div>
+                      <p class="font-semibold">Dave Li</p>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-4 py-3 text-xs">
+                  <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"> Approved </span>
+                </td>
+                <td class="px-4 py-3 text-sm">12-08-2023</td>
+              </tr>
+              <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
+                <td class="px-4 py-3">
+                  <div class="flex items-center text-sm">
+                    <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
+                      <img class="object-cover w-full h-full rounded-full" src="https://images.unsplash.com/photo-1551006917-3b4c078c47c9?ixlib=rb-1.2.1&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=200&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjE3Nzg0fQ" alt="" loading="lazy" />
+                      <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                    </div>
+                    <div>
+                      <p class="font-semibold">Julia Roberts</p>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-4 py-3 text-xs">
+                  <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"> Approved </span>
+                </td>
+                <td class="px-4 py-3 text-sm">12-08-2023</td>
+              </tr>
+              <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
+                <td class="px-4 py-3">
+                  <div class="flex items-center text-sm">
+                    <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
+                      <img class="object-cover w-full h-full rounded-full" src="https://images.unsplash.com/photo-1566411520896-01e7ca4726af?ixlib=rb-1.2.1&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=200&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjE3Nzg0fQ" alt="" loading="lazy" />
+                      <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                    </div>
+                    <div>
+                      <p class="font-semibold">Witney Houston</p>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-4 py-3 text-xs">
+                  <span class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-700"> Denied </span>
+                </td>
+                <td class="px-4 py-3 text-sm">12-08-2023</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
+          <span class="col-span-2"></span>
+
+        </div>
+      </div>
+    </div>
+    <!-- ./Client Table -->
+
+
+  </div>
   </BasePage>
 </template>
 
 <script>
+import SummaryBlock from '@/components/SummaryBlock.vue';
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    SummaryBlock
+  }
 }
 </script>
+
+<style scoped>
+
+</style>
